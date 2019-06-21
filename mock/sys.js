47e100ee743c80f70de8daae7adb43b5be2f5a2c
chenchuan <1375738574@qq.com>
2019-06-21 18:47:51 +0800

    前端表格显示

---
+++ mock/sys.js
@@ -0,0 +1,27 @@
+import mockjs from 'mockjs';
+
+function userList(req, res) {
+  const dataSource = [
+    {
+      userId: '01',
+      userName: 'jack',
+      realName: '张三',
+    },
+    {
+      userId: '02',
+      userName: 'bob',
+      realName: '小明',
+    },
+    {
+      userId: '03',
+      userName: 'she',
+      realName: '比迪',
+    },
+  ];
+
+  return res.json(dataSource);
+}
+
+export default {
+  'GET /api/userList': userList,
+};
