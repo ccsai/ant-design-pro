6d255f14a140a242594898e6cec4282972f95d4b
chenchuan <1375738574@qq.com>
2019-06-24 18:16:02 +0800

    表单加载

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,13 @@ import request from '@/utils/request';
 export async function queryUserList() {
   return request('/api/userList');
 }
+
+export async function addUser(params) {
+  return request('/api/userAdd', {
+    method: 'post',
+    data: {
+      ...params,
+      method: 'post',
+    },
+  });
+}
